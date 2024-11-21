a93a7e554c982cc2d6eb68d99c95c779f3588dc6
I1hor <ihor.babiichuk.22@pnu.edu.ua>
2024-11-21 02:36:29 +0200

    lab7

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import sys
 
 from PyQt5.QtGui import QTextLine
 from PyQt5.QtWidgets import QWidget, QApplication, QStackedWidget, QVBoxLayout, QPushButton, QLabel, QLineEdit, \
-    QComboBox, QButtonGroup, QHBoxLayout, QRadioButton
-from coefficients import COEFFICIENTS, ATTRIBUTES, EARLY_DESIGN_MULTIPLIERS, EFFORT_MULTIPLIERS, SCALE_FACTORS
+    QComboBox, QButtonGroup, QHBoxLayout, QRadioButton, QScrollArea
+from coefficients import COEFFICIENTS, ATTRIBUTES, EARLY_DESIGN_MULTIPLIERS, EFFORT_MULTIPLIERS, SCALE_FACTORS, ENV_FACTORS, LANGUAGES
 
 class MainWindow(QWidget):
 
@@ -16,19 +16,212 @@ class MainWindow(QWidget):
 
         self.button_cocomo1 = QPushButton('COCOMO 1')
         self.button_cocomo2 = QPushButton('COCOMO 2')
+        self.funct_points_btn = QPushButton('Functional Points')
         self.button_cocomo1.clicked.connect(lambda: self.stacked_widgets.setCurrentIndex(0))
         self.button_cocomo2.clicked.connect(lambda: self.stacked_widgets.setCurrentIndex(1))
+        self.funct_points_btn.clicked.connect(lambda: self.stacked_widgets.setCurrentIndex(2))
 
         self.stacked_widgets.addWidget(self.cocomo_i_page())
         self.stacked_widgets.addWidget(self.cocomo_ii_page())
+        self.stacked_widgets.addWidget(self.fp_page())
 
         layout.addWidget(self.button_cocomo1)
         layout.addWidget(self.button_cocomo2)
+        layout.addWidget(self.funct_points_btn)
         layout.addWidget(self.stacked_widgets)
 
         self.setLayout(layout)
         self.show()
 
+    from PyQt5.QtWidgets import QScrollArea
+
+    def fp_page(self):
+        page = QWidget()
+        layout = QVBoxLayout()
+
+        # Створення лейблів
+        self.ei_label = QLabel("External Inputs (EIs): ")
+        self.eo_label = QLabel("External Outputs (EOs): ")
+        self.eq_label = QLabel("External Inquiries (EQs): ")
+        self.elf_label = QLabel("Internal Logical Files (ILFs): ")
+        self.eif_label = QLabel("External Interface Files (EIFs): ")
+
+        # Створення текстових полів
+        self.ei_input = QLineEdit()
+        self.ei_input.setPlaceholderText("низький")
+        self.ei_input2 = QLineEdit()
+        self.ei_input2.setPlaceholderText("середній")
+        self.ei_input3 = QLineEdit()
+        self.ei_input3.setPlaceholderText("високий")
+
+        self.eo_input = QLineEdit()
+        self.eo_input.setPlaceholderText("низький")
+        self.eo_input2 = QLineEdit()
+        self.eo_input2.setPlaceholderText("середній")
+        self.eo_input3 = QLineEdit()
+        self.eo_input3.setPlaceholderText("високий")
+
+        self.eq_input = QLineEdit()
+        self.eq_input.setPlaceholderText("низький")
+        self.eq_input2 = QLineEdit()
+        self.eq_input2.setPlaceholderText("середній")
+        self.eq_input3 = QLineEdit()
+        self.eq_input3.setPlaceholderText("високий")
+
+        self.elf_input = QLineEdit()
+        self.elf_input.setPlaceholderText("низький")
+        self.elf_input2 = QLineEdit()
+        self.elf_input2.setPlaceholderText("середній")
+        self.elf_input3 = QLineEdit()
+        self.elf_input3.setPlaceholderText("високий")
+
+        self.eif_input = QLineEdit()
+        self.eif_input.setPlaceholderText("низький")
+        self.eif_input2 = QLineEdit()
+        self.eif_input2.setPlaceholderText("середній")
+        self.eif_input3 = QLineEdit()
+        self.eif_input3.setPlaceholderText("високий")
+
+        # Створюємо горизонтальні лейауты для кожної пари лейбл + текстове поле
+        ei_layout = QHBoxLayout()
+        ei_layout.addWidget(self.ei_label)
+        ei_layout.addWidget(self.ei_input)
+        ei_layout.addWidget(self.ei_input2)
+        ei_layout.addWidget(self.ei_input3)
+
+        eo_layout = QHBoxLayout()
+        eo_layout.addWidget(self.eo_label)
+        eo_layout.addWidget(self.eo_input)
+        eo_layout.addWidget(self.eo_input2)
+        eo_layout.addWidget(self.eo_input3)
+
+        eq_layout = QHBoxLayout()
+        eq_layout.addWidget(self.eq_label)
+        eq_layout.addWidget(self.eq_input)
+        eq_layout.addWidget(self.eq_input2)
+        eq_layout.addWidget(self.eq_input3)
+
+        elf_layout = QHBoxLayout()
+        elf_layout.addWidget(self.elf_label)
+        elf_layout.addWidget(self.elf_input)
+        elf_layout.addWidget(self.elf_input2)
+        elf_layout.addWidget(self.elf_input3)
+
+        eif_layout = QHBoxLayout()
+        eif_layout.addWidget(self.eif_label)
+        eif_layout.addWidget(self.eif_input)
+        eif_layout.addWidget(self.eif_input2)
+        eif_layout.addWidget(self.eif_input3)
+
+        # Додаємо ці горизонтальні лейауты в основний вертикальний лейаут
+        layout.addLayout(ei_layout)
+        layout.addLayout(eo_layout)
+        layout.addLayout(eq_layout)
+        layout.addLayout(elf_layout)
+        layout.addLayout(eif_layout)
+
+        # Створюємо горизонтальний лейаут для факторів середовища
+        self.ratings = [0, 1, 2, 3, 4, 5]
+        self.factor_widgets = {}
+
+        # Обгортаємо фактори середовища у QScrollArea
+        scroll_area_widget = QWidget()
+        scroll_area_layout = QVBoxLayout()
+
+        for factor in ENV_FACTORS:
+            factor_layout = QHBoxLayout()
+            factor_label = QLabel(factor)
+            factor_layout.addWidget(factor_label)
+
+            rating_combo = QComboBox()
+            rating_combo.addItems([str(rating) for rating in self.ratings])
+            factor_layout.addWidget(rating_combo)
+
+            self.factor_widgets[factor] = rating_combo
+
+            scroll_area_layout.addLayout(factor_layout)
+
+        scroll_area_widget.setLayout(scroll_area_layout)
+
+        # Створюємо QScrollArea і додаємо до основного лейауту
+        scroll_area = QScrollArea()
+        scroll_area.setWidget(scroll_area_widget)
+        scroll_area.setWidgetResizable(True)
+        layout.addWidget(scroll_area)
+
+        self.language = QComboBox()
+        self.language.addItems([item for item in LANGUAGES])
+        layout.addWidget(self.language)
+
+        calculate_btn = QPushButton("Calculate")
+        calculate_btn.clicked.connect(self.calculate_fp)
+        layout.addWidget(calculate_btn)
+
+        self.result_label = QLabel("Results will be shown here")
+        layout.addWidget(self.result_label)
+
+
+        page.setLayout(layout)
+        return page
+
+    def calculate_fp(self):
+        # Ініціалізація сум для кожної категорії
+        ei_value, eo_value, eq_value, elf_value, eif_value = 0, 0, 0, 0, 0
+
+        # Обчислення значень для введених текстових полів
+        if self.ei_input.text() != "":
+            ei_value = int(self.ei_input.text()) * 3
+        elif self.ei_input2.text() != "":
+            ei_value = int(self.ei_input2.text()) * 4
+        elif self.ei_input3.text() != "":
+            ei_value = int(self.ei_input3.text()) * 6
+
+        if self.eo_input.text() != "":
+            eo_value = int(self.eo_input.text()) * 4
+        elif self.eo_input2.text() != "":
+            eo_value = int(self.eo_input2.text()) * 5
+        elif self.eo_input3.text() != "":
+            eo_value = int(self.eo_input3.text()) * 7
+
+        if self.eq_input.text() != "":
+            eq_value = int(self.eq_input.text()) * 3
+        elif self.eq_input2.text() != "":
+            eq_value = int(self.eq_input2.text()) * 4
+        elif self.eq_input3.text() != "":
+            eq_value = int(self.eq_input3.text()) * 6
+
+        if self.elf_input.text() != "":
+            elf_value = int(self.elf_input.text()) * 7
+        elif self.elf_input2.text() != "":
+            elf_value = int(self.elf_input2.text()) * 10
+        elif self.elf_input3.text() != "":
+            elf_value = int(self.elf_input3.text()) * 15
+
+        if self.eif_input.text() != "":
+            eif_value = int(self.eif_input.text()) * 5
+        elif self.eif_input2.text() != "":
+            eif_value = int(self.eif_input2.text()) * 7
+        elif self.eif_input3.text() != "":
+            eif_value = int(self.eif_input3.text()) * 10
+
+        # Підсумок для факторів середовища
+        env_factors_sum = 0
+        for factor, combo in self.factor_widgets.items():
+            selected_rating = int(combo.currentText())  # Отримуємо вибраний рейтинг
+            env_factors_sum += selected_rating  # Додаємо до суми
+
+        total_fp = ei_value + eo_value + eq_value + elf_value + eif_value
+
+        # Розрахунок CAF і AFP
+        CAF = 0.65 + (0.01 * env_factors_sum)
+        AFP = total_fp * CAF
+        LOC = AFP * LANGUAGES[self.language.currentText()]
+
+        # Виведення результатів
+        result_text = f"Total FP: {total_fp}\nCAF: {CAF:.2f}\nAFP: {AFP:.2f}\nLOC: {LOC:.2f}"
+
+        # Оновлення лейблу з результатами
+        self.result_label.setText(result_text)
 
     def cocomo_i_page(self):
 
@@ -299,6 +492,7 @@ class MainWindow(QWidget):
 
         except Exception as e:
             self.labelRes2.setText(f"Помилка {e}")
+
 
 
 if __name__ == "__main__":
